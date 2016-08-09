b6540a3fc3456c7976a25c62356103e70038fdd5
Christian Muehlhaeuser <muesli@gmail.com>
2016-08-09 20:41:09 +0200

    Handy addButton method is handy

--- nonumy.go
+++ nonumy.go
@@ -6,6 +6,8 @@ import (
 	"crypto/sha256"
 	"crypto/sha512"
 	"fmt"
+	"hash"
+	"io"
 	"io/ioutil"
 	"os"
 
@@ -15,6 +17,22 @@ import (
 var FileToHash string
 var contents []byte
 var err error
+
+type hashFunc func() hash.Hash
+
+func addButton(name string, fn hashFunc, label *ui.Entry) *ui.Button {
+	btn := ui.NewButton(name)
+
+	// Going through the options
+	btn.OnClicked(func(*ui.Button) {
+		hasher := fn()
+		io.WriteString(hasher, string(contents))
+
+		label.SetText(fmt.Sprintf("%x", hasher.Sum(nil)))
+	})
+
+	return btn
+}
 
 func main() {
 
@@ -26,10 +44,6 @@ func main() {
 
 		OpenFileButton := ui.NewButton("Open File")
 		filepath := ui.NewLabel("")
-		md5Button := ui.NewButton("MD5")
-		sha1Button := ui.NewButton("SHA1")
-		sha256Button := ui.NewButton("SHA256")
-		sha512Button := ui.NewButton("SHA512")
 		hashsum := ui.NewEntry()
 		compareSum := ui.NewButton("Compare?")
 		compare := ui.NewEntry()
@@ -37,10 +51,12 @@ func main() {
 		box := ui.NewVerticalBox()
 		box.Append(OpenFileButton, false)
 		box.Append(filepath, false)
-		box.Append(md5Button, false)
-		box.Append(sha1Button, false)
-		box.Append(sha256Button, false)
-		box.Append(sha512Button, false)
+
+		box.Append(addButton("MD5", md5.New, hashsum), false)
+		box.Append(addButton("SHA-1", sha1.New, hashsum), false)
+		box.Append(addButton("SHA-256", sha256.New, hashsum), false)
+		box.Append(addButton("SHA-512", sha512.New, hashsum), false)
+
 		box.Append(hashsum, false)
 		box.Append(compareSum, false)
 		box.Append(compare, false)
@@ -62,20 +78,6 @@ func main() {
 			if err != nil {
 				hashsum.SetText(fmt.Sprintf("Could not read out file: %s", FileToHash))
 			}
-		})
-
-		// Going through the options
-		md5Button.OnClicked(func(*ui.Button) {
-			hashsum.SetText(fmt.Sprintf("%x", md5.Sum(contents)))
-		})
-		sha1Button.OnClicked(func(*ui.Button) {
-			hashsum.SetText(fmt.Sprintf("%x", sha1.Sum(contents)))
-		})
-		sha256Button.OnClicked(func(*ui.Button) {
-			hashsum.SetText(fmt.Sprintf("%x", sha256.Sum256(contents)))
-		})
-		sha512Button.OnClicked(func(*ui.Button) {
-			hashsum.SetText(fmt.Sprintf("%x", sha512.Sum512(contents)))
 		})
 
 		// Optionally you can compare two hashsums
